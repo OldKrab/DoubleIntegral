7e42c74c6a655ffcae604a020509da09b2f2cb49
daniil <4shushakov@gmail.com>
2021-06-02 04:23:50 +0700

    Add integrand in Monte-Carlo class

--- headers/MonteCarloIntegral.h
+++ headers/MonteCarloIntegral.h
@@ -3,12 +3,13 @@
 
 #include "geometry.h"
 #include <vector>
+#include <functional>
 
 class MonteCarloIntegral2 {
 public:
     MonteCarloIntegral2(std::vector<Point> fig, Point leftDown, Point rightUp);
 
-    db CalcIntegral(int n);
+    db CalcIntegral(int n, std::function<db(Point)> func);
 
     void GetGeneratedPoints(std::vector<Point> &inPoints, std::vector<Point> &outPoints);
 
--- source/MonteCarloIntegral.cpp
+++ source/MonteCarloIntegral.cpp
@@ -1,30 +1,29 @@
 #include "../headers/MonteCarloIntegral.h"
 #include <utility>
 
-db MonteCarloIntegral2::CalcIntegral(int n) {
-    int inFigCnt = 0;
-    inFigPoints.clear();
-    outFigPoints.clear();
-    for (int i = 0; i < n; ++i) {
-        auto point = GetRandPoint();
-        if (IsInFig(point)) {
-            inFigCnt++;
-            inFigPoints.push_back(point);
-        }
-        else
-            outFigPoints.push_back(point);
-    }
-    db limArea = (rightUp.x - leftDown.x) * (rightUp.y - leftDown.y);
-    return limArea * db(inFigCnt) / n;
-}
-
 MonteCarloIntegral2::MonteCarloIntegral2(std::vector<Point> fig, Point leftDown, Point rightUp)
         : fig(std::move(fig)), leftDown(leftDown), rightUp(rightUp) {
     srand(time(0));
 }
 
+db MonteCarloIntegral2::CalcIntegral(int n, std::function<db(Point)> func) {
+    db res = 0;
+    inFigPoints.clear();
+    outFigPoints.clear();
+    for (int i = 0; i < n; ++i) {
+        auto point = GetRandPoint();
+        if (IsInFig(point)) {
+            res += func(point);
+            inFigPoints.push_back(point);
+        } else
+            outFigPoints.push_back(point);
+    }
+    db limArea = (rightUp.x - leftDown.x) * (rightUp.y - leftDown.y);
+    return res / n * limArea;
+}
+
 Point MonteCarloIntegral2::GetRandPoint() const {
-    int mx = 1000;
+    int mx = 10000;
     db x = leftDown.x + db(rand() % mx) / mx * (rightUp.x - leftDown.x);
     db y = leftDown.y + db(rand() % mx) / mx * (rightUp.y - leftDown.y);
     return Point{x, y};
@@ -33,9 +32,9 @@ Point MonteCarloIntegral2::GetRandPoint() const {
 bool MonteCarloIntegral2::IsInFig(Point point) {
     int interCnt = 0;
     Segment cur{point, {rightUp.x, point.y}};
-    for(int i = 0; i < fig.size()-1; i++){
-        Segment line{fig[i], fig[i+1]};
-        if(cur.Intersect(line))
+    for (int i = 0; i < fig.size() - 1; i++) {
+        Segment line{fig[i], fig[i + 1]};
+        if (cur.Intersect(line))
             interCnt++;
     }
     return interCnt % 2;
@@ -45,3 +44,5 @@ void MonteCarloIntegral2::GetGeneratedPoints(std::vector<Point> &inPoints, std::
     inPoints = inFigPoints;
     outPoints = outFigPoints;
 }
+
+
